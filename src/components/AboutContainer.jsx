898180f4b62acc27bc7498f90145e072a81fe9ef
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-03-25 13:39:37 -0300

    Feat: Atualizando projeto com seção de cards

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,11 @@ const AboutContainer = () => {
         <h2>Sobre Mim</h2>
         <p>
         Sou um apaixonado por tecnologia e desenvolvimento web, buscando sempre aprender algo novo nesta jornada fantástica de conhecimentos e desafios que a tecnologia nos proporciona.
+        </p>
+        <p>
         Possuo 12 anos atuando na área de Automação Industrial em grandes empresas multinacionais, tenho facilidade de adaptação em diversos ambientes, boa comunicação e facilidade de aprendizado.
+        </p>
+        <p>
         Atualmente foco meus esforços para a área de desenvolvimento web, onde estou cursando graduação em ciência da computação e fazendo cursos voltados para a área de Front-end, Designer UX/UI e Back-end.
         </p>
     </section>
--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const ProjectsContainer = () => {
   return (
-    <div>ProjectsContainer</div>
-  )
-}
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam est esse eaque sed corporis nemo minima ab nostrum corrupti illum, accusantium optio quia? Voluptatem enim, obcaecati suscipit doloribus itaque recusandae!
+      </p>
+      <a href="#" className='btn'>
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
 
-export default ProjectsContainer
+export default ProjectsContainer;
